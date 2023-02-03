97b1c1c02168e2629a578a0150fe339219fdd41f
Popular234 <cainjose24@g.crookcountyschools.org>
2023-02-03 09:22:02 -0800

    uuuuuuuhhhhh

--- script.js
+++ script.js
@@ -1,22 +1,59 @@
 "use strict";
 
-// console.log('hello world');
+// // console.log('hello world');
 
 document.write('<h1>Objects Demo</h1>')
 
-const spaceship = {
-    engine: 'rocket',
-    color: 'green',
-    room: 'cockpit',
-    flying: function(){
-        return("My Spaceship is powered by a " + this.engine + " therfor it can fly")
-    }
+// const spaceship = {
+//     engine: 'rocket',
+//     color: 'green',
+//     room: 'cockpit',
+//     flying: function(){
+//         return("My Spaceship is powered by a " + this.engine + " therfor it can fly")
+//     }
+// }
+
+// console.log(spaceship.color);
+
+// // document.write('<h2> I have a spaceship it is ', spaceship.color, ' my spaceship also has a ', spaceship.room, spaceship.flying(), '</h2>');
+
+// document.write('<h2>', spaceship.flying, '</h2>')
+
+// document.write('<h2>', spaceship.flying(), '</h2>')
+
+// function Car (make, model, color, year){
+//     this.make = make;
+//     this.model = model;
+//     this.color = color;
+//     this.year = year;
+// }
+
+// const myCar = new Car("ford", "F150", "Blue", "2016");
+// const sammyCar = new Car("Dodge", "Ram 1500", "White", "2017");
+// const kierrasCar = new Car("Toyota", "Highlander", "Black", "2020");
+// console.log(myCar);
+// console.log(sammyCar);
+// console.log(kierrasCar);
+
+function Icecream(name, flavor, dish, size){
+    this.name = name;
+    this.flavor = flavor;
+    this.dish = dish;
+    this.size = size;
 }
 
-console.log(spaceship.color);
+Icecream.prototype.topping = 'sprinkles';
+Icecream.prototype.string = function(){
+    return "My name is " + this.name + " I like " + this.flavor + " icecream my preference would be to have it in " + this.dish + " and have it be " + this.size
+}
 
-// document.write('<h2> I have a spaceship it is ', spaceship.color, ' my spaceship also has a ', spaceship.room, spaceship.flying(), '</h2>');
+const mrmummIcecream = new Icecream('mr mumm', 'Caramel', 'Bowl', 'Large');
+const joshsIcecream = new Icecream('Josh', 'cookie dough', 'blizzard', 'large')
+const cainsIcecream = new Icecream('Cain', 'Reeses', 'Blizzard', 'mini')
+const jrsIcecream = new Icecream('Jr', 'Vanilla', 'cone', 'large')
 
-document.write('<h2>', spaceship.flying, '</h2>')
-
-document.write('<h2>', spaceship.flying(), '</h2>')
+console.log(mrmummIcecream);
+console.log(joshsIcecream);
+console.log(cainsIcecream);
+console.log(jrsIcecream, Icecream.prototype.string);
+document.write('<h2>',jrsIcecream.string(), '</h2>')
